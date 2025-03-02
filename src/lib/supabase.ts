737427ed08771dc68de659af50c77eb00d1f9e42
Yash Kandalkar <yashsk.yk@gmail.com>
2025-03-02 10:34:48 +0530

    fix: auth error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -33,7 +33,9 @@ export const signOut = async () => {
 export const getCurrentUser = async () => {
   const {
     data: { user },
+    error,
   } = await supabase.auth.getUser();
+  if (error) throw error;
   return user;
 };
 
